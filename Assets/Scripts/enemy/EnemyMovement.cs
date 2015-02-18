303fb203b86a79cd5be1f666b31a4a89ef869430
aaron seng <seng.aaron@gmail.com>
2015-02-18 02:06:06 +0200

    Enemies added

---
+++ Assets/Scripts/enemy/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	private Transform player;
+	private NavMeshAgent navigationAgent;
+
+	private void Awake() {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		navigationAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	private void Update() {
+		navigationAgent.SetDestination(player.position);
+	}
+}
